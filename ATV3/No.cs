11b2bd245f2b19da2080a694d3d75391a68f21cc
daniel10dp <55281079+daniel10dp@users.noreply.github.com>
2021-03-31 19:08:58 -0300

    Add files via upload

---
+++ ATV3/No.cs
@@ -0,0 +1,16 @@
+namespace ATV3
+{
+    public class No
+    {
+        public No noEsquerdo;
+		public int info;
+		public No noDireito;
+
+		public No(int info)
+		{
+			this.noEsquerdo = null;
+			this.info = info;
+			this.noDireito = null;
+		}
+    }
+}
---
+++ ATV3/Percurso.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+namespace ATV3
+{
+    public class Percurso : ArvoreBinaria
+    {
+        public bool Inserirpercurso(List<int> pre_ordem, List<int> em_ordem){
+
+            while(pre_ordem.Count != 0) {
+                Inserir(pre_ordem [0]);
+                int aux = pre_ordem[0];
+                pre_ordem.RemoveAt(0);
+
+                int k = 0;
+                foreach(int c in em_ordem){
+                     if(c == aux){
+                         em_ordem.RemoveAt(k);
+                         break;
+                     }
+                     else k++;
+                }
+            }
+
+            if(pre_ordem.Count == 0 && em_ordem.Count == 0){
+                    Console.WriteLine("\n O percurso foi inserido!\n");
+                    return true;
+            }else   return false;
+            
+
+        }
+
+
+    } 
+}
